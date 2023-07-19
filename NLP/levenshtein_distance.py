c4740e27900c43be8e310249f606dd6df6411624
gilpasternak35 <gilpasternak35@gmail.com>
2023-07-19 16:06:14 -0700

    weighted dp solution implemented

--- NLP/levenshtein_distance.py
+++ NLP/levenshtein_distance.py
@@ -1,45 +1,42 @@
 import argparse
 from typing import List
+import numpy as np
 
 
-def levenshtein_distance(string1: str, string2: str) -> int:
+def levenshtein_distance(string1: str, string2: str, subs_weight:float) -> int:
     """
-    Returns Levenshtein distance between two given strings
+    A DP Style soltuion for levenshtein edit distance
     """
-    # ensuring string2 is longer of the two strings, swapping if this is not the case
-    if len(string1) > len(string2):
-        string1, string2 = string2, string1
+    # two pronged array with strings as indices
+    table = np.zeros((len(string1), len(string2)))
+    
+    # iterating through dp table and populating entries
+    for i in range(table.shape[0]):
+        for j in range(table.shape[1]):
 
-    # computing necessary additions
-    necessary_additions = len(string2) - len(string1)
-
-    # computing naive solution - pure substitutions and necessary additions, with aligned subs having cost 0
-    naive_solution = max(len(string1), len(string2)) - compute_aligned_chars(string1, string2)
-
-    # to maintain best improvement
-    best_improvement  = 0
-
-    # computing offset by trying every possible offset given an insertion (at most k insertions make sense at all, where k is naive solution)
-    # algorithm should run in quadratic time, which is worse than DP. However, shows an interesting thought exercise
-    for j in range(len(string1)):
-        # temporarily modifying string1 so as to see how many commonalities we can create
-        # field theory: should be able to have insert characters only in beginning. Alignment anywhere will result from insertion at beginning
-        string2 = string1[:j] + string2
-        best_improvement = max(best_improvement, compute_aligned_chars(string1, string2) - j)
-
-
-    # best solution is naive = best improvement we could make by aligning common chars
-    return naive_solution - best_improvement
-        
-
+            # special case where cannot rely on predecessor, so must evaluate match. Best option is either subs or deletion+insertion
+            if i == 0 and j == 0:
+                table[i][j] = 0 if string1[i] == string2[j] else min(subs_weight, 2)
             
+            # otherwise, checking best path from a predecessor
+            else:
+                # if common char, don't need to use a formal substitution to make transition
+                if string1[i] == string2[j]:
+                    # maintaining sub cost
+                    subs_cost = table[i-1, j-1] if i > 0 and j > 0 else np.infty
+                else:
+                    subs_cost = table[i-1, j-1] + subs_weight if i > 0 and j > 0 else np.infty
 
+                # computing min addition, subtraction cost
+                del_cost = table[i-1, j] + 1 if i > 0 else np.infty
+                add_cost = table[i, j-1] + 1 if j > 0 else np.infty
 
-def compute_aligned_chars(s1: str, s2: str):
-    """
-    Returns number of corresponding positional characters in two strings
-    """
-    return sum([s1[i] == s2[i] for i in range(min(len(s1), len(s2)))])
+                # updating table with the minimum of all three costs
+                table[i,j] = min(subs_cost, del_cost, add_cost)
+
+    # returning final entry
+    print(table)
+    return table[-1][-1]
 
 
 if __name__ == "__main__":
@@ -47,9 +44,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("Levenshtein Distance", description="Computes Levenshtein Distance between two given strings")
     parser.add_argument("-s1", type=str, help="First string", required=False, default="Mary had a little lamb")
     parser.add_argument("-s2", type=str, help="Second string", required=False, default="Mary had a large lamb, wow what a world")
+    parser.add_argument("-s-weight", type=float, help="The weight of a substition", required=False, default=1)
     args = parser.parse_args()
 
     # printing Levenshtein distance
-    print(levenshtein_distance(args.s1, args.s2))
+    print(levenshtein_distance(args.s1, args.s2, args.s_weight))
 
 
